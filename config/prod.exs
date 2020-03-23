ffa4a4af76ad380296a23888fbc70f0001064a93
Joel C <joel@vorce.se>
2020-03-23 20:15:46 +0100

    Set host to playlistlog.vorce.se

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :playlist_log, PlaylistLogWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "playlistlog.vorce.se", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
